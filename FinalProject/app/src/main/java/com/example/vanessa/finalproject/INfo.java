d64232b8869b6d4aee8894fed33b308bc67e5515
VanessaC1995 <38332086+VanessaC1995@users.noreply.github.com>
2018-04-13 22:01:55 +0100

    project
    project

---
+++ FinalProject/app/src/main/java/com/example/vanessa/finalproject/INfo.java
@@ -0,0 +1,47 @@
+package com.example.vanessa.finalproject;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+public class INfo extends AppCompatActivity {
+
+    ImageButton btnFacebook, btnTwitter, btnUUSU;
+    //information screen to show info about the club and to access clubs social media pages
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info);
+
+        btnFacebook=(ImageButton)findViewById(R.id.btnFacebook);
+        btnFacebook.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent= new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.facebook.com/uujbadminton/"));
+                startActivity(intent);
+            }
+        });
+
+        btnTwitter=(ImageButton)findViewById(R.id.btnTwitter);
+        btnTwitter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent= new Intent(Intent.ACTION_VIEW, Uri.parse("https://twitter.com/uususport"));
+                startActivity(intent);
+            }
+        });
+
+        btnUUSU=(ImageButton)findViewById(R.id.btnUUSU);
+        btnUUSU.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent= new Intent(Intent.ACTION_VIEW, Uri.parse("http://uususport.org/clubs/badminton-club/"));
+                startActivity(intent);
+            }
+        });
+
+    }
+}
